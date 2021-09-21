f26c0332772536fe86a0cfa81d9cf844a222eae6
Kia Storm <kia@kormsen.com>
2021-09-21 16:34:47 +0100

    Initial commit

--- test/pages/showcase.test.tsx
+++ test/pages/showcase.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import Showcase from '../../pages/design-system'
 
-describe('Home page', () => {
+describe('Showcase page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = render(<Showcase />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
+    const { getByText } = render(<Showcase />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText('Test Button'))
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
